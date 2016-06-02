0b839caee0fe091d1ede3697d14de1aaafeea643
ReganLayman <regan.layman@gmail.com>
2016-06-01 21:29:51 -0400

    adding a method to an object with dice rolling challenge

--- 1_Dice/1_dice_start/dice.js
+++ 1_Dice/1_dice_start/dice.js
@@ -1,9 +1,9 @@
-function diceRoll() {
+
+
+var dice = {
+	roll: function () {
   var sides = 6;
   var randomNumber = Math.floor(Math.random() * sides) + 1;
   console.log(randomNumber);
 }
-
-var dice = {
-
 }
