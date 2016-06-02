c2d8eba2f84f08f8598d77dd5ea7fc41fc34163e
ReganLayman <regan.layman@gmail.com>
2016-06-01 20:55:42 -0400

    object methods this

--- this/js/app.js
+++ this/js/app.js
@@ -1,6 +1,8 @@
-function helloWorld() {
-  console.log(“Hello world!”);
-  console.log(this);
-};
-
-helloWorld()
+var Portland = {
+bridges: 12,
+airport: 1, 
+soccerTeams: 1,
+logNumberOfBridges: function() {
+   console.log(“there are “ + this.bridges + “ bridges in Portland!”)
+  }
+}
