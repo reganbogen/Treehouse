4f08ae0eadcbff2f0eaf37531a728b653b28e453
ReganLayman <regan.layman@gmail.com>
2016-06-26 15:00:26 -0400

    change request to req, response to res

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -1,3 +1,13 @@
 "use strict";
 
 var express = require("express");
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("<h1>I love treehouse!</h1>");
+});
+
+app.listen(3000, function(){
+	console.log("The frontend server is running on port 3000!")
+});
