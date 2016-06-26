22c95edfb0e00bc98d243b726f314d1028c7ef61
ReganLayman <regan.layman@gmail.com>
2016-06-26 15:10:48 -0400

    project files for 2nd part of express basics course

---
+++ express-basics2/src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("<h1>I Love Treehouse!</h1>");
+});
+
+
+app.listen(3000, function(){
+	console.log("The frontend server is running on port 3000!")
+});
