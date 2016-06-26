a32771d2ebe16af0f8d1b9e9327e0caa1d55baf8
ReganLayman <regan.layman@gmail.com>
2016-06-26 15:12:12 -0400

    added .get blog app

--- express-basics2/src/app.js
+++ express-basics2/src/app.js
@@ -8,6 +8,9 @@ app.get('/', function(req, res){
 	res.send("<h1>I Love Treehouse!</h1>");
 });
 
+app.get('/blog', function(req, res){
+
+});
 
 app.listen(3000, function(){
 	console.log("The frontend server is running on port 3000!")
