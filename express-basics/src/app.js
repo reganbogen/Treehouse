06ecd104a60bc970decc09cb6b96fe6efa74fb3f
ReganLayman <regan.layman@gmail.com>
2016-06-26 15:10:21 -0400

    final save, update all the responses to res

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res){
 	res.send("<h1>I love treehouse!</h1>");
 });
 
+app.get('/blog', function(req, res){
+	
+});
+
 app.listen(3000, function(){
 	console.log("The frontend server is running on port 3000!")
 });
