818062d26603c0dd14e069958424de7eb0ea642f
ReganLayman <regan.layman@gmail.com>
2016-06-28 17:23:13 -0400

    continuing res.render method

--- express-basics6/src/app.js
+++ express-basics6/src/app.js
@@ -18,8 +18,8 @@ app.get('/blog/:title?', function(req, res){
 		res.status(503);
 		res.send("This page is under construction!");
 	} else {
-		var post = posts[title];
-		res.send(post);
+		var post = posts[title] || {};
+		res.render('post', {post: post});
 	}
 });
 
