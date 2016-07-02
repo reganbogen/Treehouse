6753b3b1affa36ad6b0e5e71c3fd0ba7319c64d7
ReganLayman <regan.layman@gmail.com>
2016-07-02 14:26:46 -0400

    made concatScripts a parameter of the minify function, removed it from the build function, made the default build so i can just run gulp to build application

--- gulp_basics/gulpfile.js
+++ gulp_basics/gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
     maps = require('gulp-sourcemaps');
 
 gulp.task("concatScripts", function() {
-    gulp.src([
+   return gulp.src([
         'js/jquery.js',
         'js/sticky/jquery.sticky.js',
         'js/main.js'
@@ -19,23 +19,21 @@ gulp.task("concatScripts", function() {
     .pipe(gulp.dest('js'));
 });
 
-gulp.task("minifyScripts", function() {
-	gulp.src("js/app.js")
+gulp.task("minifyScripts", ["concatScripts"], function() {
+	return gulp.src("js/app.js")
 		.pipe(uglify())
 		.pipe(rename('app.min.js'))
 		.pipe(gulp.dest('js'));
 });
 
 gulp.task('compileSass', function() {
-  gulp.src("scss/application.scss")
+  return gulp.src("scss/application.scss")
       .pipe(maps.init())
       .pipe(sass())
       .pipe(maps.write('./'))
       .pipe(gulp.dest('css'));
 })
 
-gulp.task('build', ['concatScripts', 'minifyScripts', 'compileSass'])
+gulp.task("build", ['minifyScripts', 'compileSass']);
 
-gulp.task("default", ["hello"], function() {
-    console.log("the default task!!!!");
-});
+gulp.task("default", ["build"]);
