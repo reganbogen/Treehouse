8f97b263f2d5645d7704739e31a64c9abb6bbd28
ReganLayman <regan.layman@gmail.com>
2016-07-06 13:35:35 -0400

    corrected placedment of ng-controller div, added second /div, fixed label class editing label on line 14

--- angular_todo/scripts/app.js
+++ angular_todo/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
 .controller('mainCtrl', function(scope) {
 	$scope.helloWorld = function() {
 		console.log("hello there: This is the helloworld controller function, in the mainCtrl: ");
