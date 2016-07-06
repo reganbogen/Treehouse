5e91652301907d0b62e61b3e3133795a200e3507
ReganLayman <regan.layman@gmail.com>
2016-07-06 14:55:57 -0400

    still not working correctly, but added an array to app.js and called it in for edit

--- angular_todo/scripts/app.js
+++ angular_todo/scripts/app.js
@@ -1,6 +1,17 @@
 angular.module("todoListApp", [])
-.controller('mainCtrl', function(scope) {
+
+.controller('mainCtrl', function($scope) {
 	$scope.helloWorld = function() {
-		console.log("hello there: This is the helloworld controller function, in the mainCtrl: ");
+		console.log("hello there: This is the helloworld controller function, in the mainCtrl!");
 	};
+
+		$scope.todos = [
+			{"name": "clean the house"},
+			{"name": "water the dog"},
+			{"name": "feed the lawn"},
+			{"name": "pay dem bills"},
+			{"name": "run"},
+			{"name": "swim"}
+		]
+		
 });
