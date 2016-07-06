d6cd86aa441bc02adf145874840287a68859a3df
ReganLayman <regan.layman@gmail.com>
2016-07-06 16:34:13 -0400

    still not working, working with services

--- angular_todo/scripts/app.js
+++ angular_todo/scripts/app.js
@@ -1,17 +1,42 @@
 angular.module("todoListApp", [])
 
-.controller('mainCtrl', function($scope) {
+.controller('mainCtrl', function($scope, dataService) {
 	$scope.learningNgChange = function() {
 		console.log("An input changed.");
 	};
 
-		$scope.todos = [
-			{"name": "clean the house"},
-			{"name": "water the dog"},
-			{"name": "feed the lawn"},
-			{"name": "pay dem bills"},
-			{"name": "run"},
-			{"name": "swim"}
-		]
+	$scope.helloWorld = dataService.helloWorld;
+
+	dataService.getTodos(function(response) {
+		console.log(response.data);
+		$scope.tods = response.data;
+	});
+
+	$scope.deleteTodo = function(todo, $index) {
+		dataService.deleteTodo(todo);$scope.todos.splice($index, 1);
+	};
+
+	$scop.saveTodo = function(todo) {
+		dataService.saveTodo(todo);
+	}
+})
+
+.service('dataService', function($http) {
+	this.helloWorld = function() {
+		console.log("this is the data service's method!");
+	};
+
+	this.getTodos = function(callback) {
+		$http.get('mock/todos.json')
+		.then(callback)
+	};
+
+	this.deleteTodo = function(todo) {
+		console.log("The" + todo.name + "has been deleted!")
+		//other logic
+	};
+	this.saveTodos = function(todo) {
+		console.log("Ths " + todo.name + " dodo has been saved!");
+	};
 
 });
